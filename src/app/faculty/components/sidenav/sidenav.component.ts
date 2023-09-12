21c88a0edfc6c49383f85e5aaa094d2c26856ff7
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-09-12 10:16:32 +0530

    alignments of codes

--- src/app/faculty/components/sidenav/sidenav.component.ts
+++ src/app/faculty/components/sidenav/sidenav.component.ts
@@ -3,31 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
-  opened=true
+  opened = true;
   selectedIndex: number | null = 0;
-  public component="qgen"
-  constructor(){}
-  ngOnInit(): void {
-
-  }
+  public component = 'qgen';
+  constructor() {}
+  ngOnInit(): void {}
   changeBackgroundColor(index: number): void {
     this.selectedIndex = index;
   }
-  qgen(){
-    this.component="qgen"
+  qgen() {
+    this.component = 'qgen';
   }
-  editor(){
-    this.component="editor"
+  editor() {
+    this.component = 'editor';
   }
-  askeminence(){
-    this.component="askeminence"
-  }
-
-  history(){
-    this.component="history"
+  askeminence() {
+    this.component = 'askeminence';
   }
 
+  history() {
+    this.component = 'history';
+  }
 }
