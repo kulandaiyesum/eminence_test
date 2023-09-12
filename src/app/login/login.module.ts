1862e64894debf81f4880631589ce276a6bb9a5f
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-09-12 15:48:15 +0530

    login_screen

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,18 +6,22 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginLayoutComponent } from './pages/login-layout/login-layout.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
     ForgotPasswordComponent,
-    LoginLayoutComponent
+    LoginLayoutComponent,
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
