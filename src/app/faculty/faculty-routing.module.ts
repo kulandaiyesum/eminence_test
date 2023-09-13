135ebdd20603a847ccf2a5340c8e75df22484f7a
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-09-13 16:12:13 +0530

    login function

--- src/app/faculty/faculty-routing.module.ts
+++ src/app/faculty/faculty-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AuthGuard } from '../shared/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: 'qgen',
     component: SidenavComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FacultyRoutingModule { }
+export class FacultyRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   localdomain: 'http://localhost:3000',
+  secretKey:'39cb92a0d12600d10b49b90049cd4a11d34185ba4324bd9210e31d78391d3f6a'
 };
