dcb2759cb9c8afdfa083a16b2c491472166f171d
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-09-14 17:53:13 +0530

    auth login module

---
+++ src/app/auth/service/authloginservice.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthloginserviceService {
+
+  loginURL = environment.localdomain + 'logins/';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private http: HttpClient) {}
+
+  loginAuthenticate(data: any) {
+    return this.http.post(`${this.loginURL}save`, data, {
+      headers: this.headers,
+    });
+  }
+}
