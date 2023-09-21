2709082ec1dd88c4c2363e44ba20c5c5ae66368c
DharanidhaSebastin <dharanidhasebastin07@gmail.com>
2023-09-21 15:14:27 +0530

    Minor changes

--- src/app/master/components/package-popup/package-popup.component.ts
+++ src/app/master/components/package-popup/package-popup.component.ts
@@ -1,9 +1,8 @@
-import { Component, Inject, ViewChild } from '@angular/core';
+import { Component, Inject, Renderer2, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Package } from '../../model/package.class';
 import { PackageService } from '../../service/package.service';
-
 
 @Component({
   selector: 'app-package-popup',
@@ -13,8 +12,7 @@ import { PackageService } from '../../service/package.service';
 export class PackagePopupComponent {
   packageForm: FormGroup;
   visibleUpdate: boolean = false;
-  // type: string = '';
-  public package : Package
+  public package: Package;
 
   @ViewChild('packageForm', { static: false }) packageFormDirective: any;
 
@@ -22,20 +20,9 @@ export class PackagePopupComponent {
     private formBuilder: FormBuilder,
     private dialogRef: MatDialogRef<PackagePopupComponent>,
     private packageService: PackageService,
+    private renderer: Renderer2,
     @Inject(MAT_DIALOG_DATA) private data: any
-  ) {
-    // if (data != null) {
-    //   this.visibleUpdate = true;
-    //   this.packageModel._id = data._id;
-    //   this.packageModel.packageName = data.packageName;
-    //   this.packageModel.questionsCount = data.questionsCount;
-    //   this.packageModel.amount = data.amount;
-    //   this.packageModel.rate = data.rate;
-
-    //   this.packageModel.durationType = data.durationType;
-    //   this.packageModel.durationCount = data.durationCount;
-    // }
-  }
+  ) {}
 
   ngOnInit(): void {
     if (this.data && this.data.packageData) {
@@ -46,9 +33,6 @@ export class PackagePopupComponent {
     }
     this.initForm();
   }
-
-
-
 
   initForm() {
     this.packageForm = this.formBuilder.group({
@@ -61,78 +45,22 @@ export class PackagePopupComponent {
     });
   }
 
-
   closeDialog() {
     this.dialogRef.close();
   }
 
   typeChanged() {
-    console.log("run")
-    // You can add logic here to enable/disable other fields based on the selected type
-    // if (this.selectedType === 'B2B') {
-    //   console.log("b2b")
-    //   // If 'B2B' is selected, enable the fields
-    //   this.packageForm.controls['questionsCount'].enable();
-    //   this.packageForm.controls['amount'].enable();
-    //   this.packageForm.controls['rate'].enable();
-    //   this.packageForm.controls['duration_type'].enable();
-    //   this.packageForm.controls['duration_count'].enable();
-    // } else {
-    //   console.log("b2c")
-    //   // If 'B2C' or any other type is selected, disable the fields
-    //   this.packageForm.controls['questionsCount'].disable();
-    //   this.packageForm.controls['amount'].disable();
-    //   this.packageForm.controls['rate'].disable();
-    //   this.packageForm.controls['duration_type'].disable();
-    //   this.packageForm.controls['duration_count'].disable();
-    // }
+    console.log('run');
   }
 
-
   onPackageSubmit() {
-    // console.log(this.package);
+    console.log(this.package, 'dddddddddddddddddddddd');
 
-    // this.packageForm.controls['questionsCount'].setValue(this.package.questionsCount);
-    // this.packageForm.controls['packageName'].setValue(this.packageModel.packageName);
-    // this.packageForm.controls['amount'].setValue(
-    //   this.package.amount
-    // );
-    // this.packageForm.controls['rate'].setValue(this.packageModel.rate);
-
-
-    // this.packageForm.controls['durationType'].setValue(
-    //   this.packageModel.durationType
-    // );
-    // this.packageForm.controls['durationCount'].setValue(
-    //   this.packageModel.durationCount
-    // );
-
-    // if (this.packageForm.valid) {
-    //   console.log("submiited");
-
-    //   const packageData: Package = this.packageForm.value;
-    //   console.log(packageData);
-    //   this.closeDialog();
-
-    //   // Send packageData to the backend using your package service
-    //   this.packageService.createPackage(packageData).subscribe(
-    //     (response: any) => {
-    //       console.log(response);
-    //     },
-    //     (error) => {
-    //       console.error('Failed to create package', error);
-    //     }
-    //   );
-    // }
-    console.log(this.package,"dddddddddddddddddddddd");
-
-    this.packageService.createPackage(this.package).subscribe((data:any)=>{
+    this.packageService.createPackage(this.package).subscribe((data: any) => {
       this.dialogRef.close();
       console.log(data);
-
-    })
-
-
+      this.resetForm();
+    });
   }
 
   resetForm() {
@@ -142,13 +70,18 @@ export class PackagePopupComponent {
 
   updatePackage() {
     console.log('submitted');
-    // Set form values from package data
     this.packageForm.controls['packageName'].setValue(this.package.packageName);
-    this.packageForm.controls['questionsCount'].setValue(this.package.questionsCount);
+    this.packageForm.controls['questionsCount'].setValue(
+      this.package.questionsCount
+    );
     this.packageForm.controls['amount'].setValue(this.package.amount);
     this.packageForm.controls['rate'].setValue(this.package.rate);
-    this.packageForm.controls['durationType'].setValue(this.package.durationType);
-    this.packageForm.controls['durationCount'].setValue(this.package.durationCount);
+    this.packageForm.controls['durationType'].setValue(
+      this.package.durationType
+    );
+    this.packageForm.controls['durationCount'].setValue(
+      this.package.durationCount
+    );
     this.packageForm.controls['_id'].setValue(this.package._id);
 
     if (this.packageForm.valid) {
@@ -168,6 +101,4 @@ export class PackagePopupComponent {
   onNoClick(): void {
     this.dialogRef.close();
   }
-
-
 }
