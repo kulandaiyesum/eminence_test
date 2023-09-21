e8b4e637a1fa7d93d71444b73caa97b1c0d8f974
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-09-21 16:04:53 +0530

    Forgot password design

--- src/app/login/components/forgot-password/forgot-password.component.ts
+++ src/app/login/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent {
+  ForgotPasswordForm: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ){}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm(){
+    this.ForgotPasswordForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      conformpassword: ['', [Validators.required, Validators.minLength(8)]],
+    },
+    {
+      validators: this.passwordMatchValidator,
+    }
+    );
+  }
+
+  onSubmit(){
+    if (this.ForgotPasswordForm.valid) {
+      console.log(this.ForgotPasswordForm.value);
+    }
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const conformpassword = group.get('conformpassword')?.value;
+    if (password !== conformpassword) {
+      group.get('conformpassword')?.setErrors({ passwordMismatch: true });
+    } else {
+      group.get('conformpassword')?.setErrors(null);
+    }
+  }
 }
