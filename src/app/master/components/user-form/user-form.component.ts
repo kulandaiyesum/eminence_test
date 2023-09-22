77b2f5438109a490409f1cd76ca46d05eff7ba72
kulandai-yesu <kulandaiy@datapattern.ai>
2023-09-22 15:29:29 +0530

    user ui style

--- src/app/master/components/user-form/user-form.component.ts
+++ src/app/master/components/user-form/user-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { UserService } from '../../service/user.service';
 import { InstituteserviceService } from '../../service/instituteservice.service';
 import { RoleService } from '../../service/role.service';
@@ -9,6 +9,9 @@ import { User } from '../../model/user';
 import { passwordMatchValidator } from 'src/app/shared/validators/custom-validator';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
+import { Role } from '../../model/role';
+import { Topic } from '../../model/topic';
+import { Institution } from '../../model/institution.class';
 
 @Component({
   selector: 'app-user-form',
@@ -28,10 +31,11 @@ export class UserFormComponent implements OnInit {
     topicId: undefined,
   };
 
-  roles: any;
-  topics: any;
-  intitutions: any;
+  roles: Role[] = [];
+  topics: Topic[] = [];
+  intitutions: Institution[] = [];
   passwordVisible: boolean = true;
+  selectedRole: string = '';
   constructor(
     private userService: UserService,
     private toastr: ToastrService,
@@ -39,7 +43,7 @@ export class UserFormComponent implements OnInit {
     private roleService: RoleService,
     private topicService: TopicService,
     public dialogRef: MatDialogRef<UserFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: User
   ) {
     this.userForm = new FormGroup(
       {
@@ -82,18 +86,27 @@ export class UserFormComponent implements OnInit {
       this.userObject.role = this.data.role._id;
       this.userObject.topicId = this.data.topicId._id;
       this.userObject.institutionId = this.data.institutionId._id;
-    }
-    if (this.data !== null) {
       this.passwordVisible = false;
       this.userForm.get('password').disable();
       this.userForm.get('confirmPassword').disable();
+      this.selectedRole = this.data.role.role;
     }
     this.userForm.get('role').valueChanges.subscribe((selectedRole) => {
-      if (selectedRole === '650c7bf56d16fcce04fad515') {
+      const roleObj = this.roles.find((role) => role._id === selectedRole);
+      if (
+        roleObj === undefined &&
+        this.data !== null &&
+        this.selectedRole === 'FACULTY'
+      ) {
+      } else if (roleObj !== undefined) {
+        this.selectedRole = roleObj?.role;
         this.userForm.get('institutionId').enable();
+        console.log(this.userObject.institutionId, 'nan');
       } else {
+        this.selectedRole = '';
         this.userForm.get('institutionId').disable();
         this.userObject.institutionId = undefined;
+        console.log(this.userObject.institutionId);
       }
     });
   }
@@ -117,7 +130,6 @@ export class UserFormComponent implements OnInit {
   updateUserMaster() {
     this.userService.updateUserMaster(this.userObject).subscribe(
       (res: any) => {
-        console.log(res);
         this.userForm.reset();
         this.toastr.success(res.message, '', {
           timeOut: 3000,
@@ -145,7 +157,6 @@ export class UserFormComponent implements OnInit {
   getInstitution() {
     this.institutionService.getAllInstitute().subscribe((res: any) => {
       this.intitutions = res.result;
-      // this.uniqueInstitutions = this;
     });
   }
   onNoClick(): void {
