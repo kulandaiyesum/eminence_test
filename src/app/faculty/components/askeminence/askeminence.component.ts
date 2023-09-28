b909421eddfa2779a52ef329a2b5fdb09b1e34d4
kulandai-yesu <kulandaiy@datapattern.ai>
2023-09-28 18:07:41 +0530

    ask eminence update

--- src/app/faculty/components/askeminence/askeminence.component.ts
+++ src/app/faculty/components/askeminence/askeminence.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-askeminence',
   templateUrl: './askeminence.component.html',
-  styleUrls: ['./askeminence.component.scss']
+  styleUrls: ['./askeminence.component.scss'],
 })
 export class AskeminenceComponent {
-
+  public prompts: string[] = [
+    'Summarize the steps of an action potential into a neat table.',
+    'List the adverse effects of each class of anti-arrhythmics drugs.',
+    'Make a content outline for endocrine pathology.',
+    'Create helpful mnemonics to remember the interleukins.',
+  ];
 }
