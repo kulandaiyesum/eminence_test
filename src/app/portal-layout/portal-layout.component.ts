39dcc7d0cdbb8c5b3bba3bda95ca68db00d01a8a
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-10-04 11:56:16 +0530

    Vetter questions number

--- src/app/portal-layout/portal-layout.component.ts
+++ src/app/portal-layout/portal-layout.component.ts
@@ -14,6 +14,7 @@ import { environment } from 'src/environments/environment';
 export class PortalLayoutComponent implements OnInit {
   secretKey: string = environment.secretKey;
   public role: string = '';
+  questionNumbers = [1, 2, 3, 4, 5, 6, 7];
   constructor(private router: Router, private rsaService: RsaService) {}
   firstName: string = '';
   ngOnInit(): void {
