d7fd461673d6a7c27a66c41df3b100102a2d7807
DharanidhaSebastin <dharanidhasebastin07@gmail.com>
2023-10-17 10:49:20 +0530

    Minor changes made - Dharanidha Commit

--- src/app/master/components/add-subscription/add-subscription.component.ts
+++ src/app/master/components/add-subscription/add-subscription.component.ts
@@ -149,13 +149,21 @@ export class AddSubscriptionComponent implements OnInit {
     // );
 
     if (endDate) {
-      const year = endDate.getFullYear();
-      const month = String(endDate.getMonth() + 1).padStart(2, '0');
-      const day = String(endDate.getDate()).padStart(2, '0');
-      this.calculatedEndDate = `${month}/${day}/${year}`;
+      const formattedEndDate = this.formatDate(endDate);
+      this.calculatedEndDate = formattedEndDate;
     } else {
       this.calculatedEndDate = '';
     }
+  }
+
+  formatDate(date: Date): string {
+    if (date) {
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      return `${month}-${day}-${year}`;
+    }
+    return '';
   }
 
   getPackageData() {
