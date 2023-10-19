a81afd73910d11a05f2432954ae505304ad6df31
Jawahar <jawahar.as@shinelogics.com>
2023-10-19 21:24:18 +0530

    update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  localdomain: 'http://localhost:3003/api/',
+  localdomain: 'https://eminenceai.eopsys.com/api/',
   secretKey: '39cb92a0d12600d10b49b90049cd4a11d34185ba4324bd9210e31d78391d3f6a',
 };
