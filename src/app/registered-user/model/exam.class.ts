7737ea628e51d23a44dc99952844e746f9fa6fc5
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-11-02 18:28:07 +0530

    Exam functions

---
+++ src/app/registered-user/model/exam.class.ts
@@ -0,0 +1,9 @@
+export class Exam {
+  studentId: string;
+  questionId: string;
+  selectedAnswer: string;
+  correctAnswer: string;
+  Flag: string;
+  createdAt: string;
+  createdBy: string;
+}
---
+++ src/app/registered-user/service/exam.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExamService {
+  public baseUrl = environment.localdomain + 'questions';
+
+  constructor(private http: HttpClient) {}
+
+  examSubmit(data){
+    return this.http.post(this.baseUrl+data.studentId,data)
+  }
+}
