9261a41a5c4fce4db199aa38e1d61be33d347ee0
kulandai-yesu <kulandaiy@datapattern.ai>
2023-11-02 18:40:09 +0530

    form bugs fixed and added validation

--- src/app/registered-user/components/build-test/build-test.component.ts
+++ src/app/registered-user/components/build-test/build-test.component.ts
@@ -49,6 +49,7 @@ export class BuildTestComponent {
   subjectList: Subject[] = [];
   systemList: System[];
   subsystemList: SubSystem[] = [];
+  examMode: string = '';
 
   constructor(
     private rsaService: RsaService,
@@ -70,23 +71,50 @@ export class BuildTestComponent {
 
   ngOnInit(): void {
     this.qbankObject = new Qbank();
-    this.qbankForm = this.fb.group({
-      _id: [''],
-      subjectId: ['', Validators.required],
-      systemId: ['', Validators.required],
-      subsystemId: ['', Validators.required],
-      type0: false,
-      type1: false,
-      type2: false,
-      type3: false,
-      modes: [],
-      questionsCount: [0, [Validators.required, this.maxQuestionsValidator]],
-    });
+    this.qbankForm = this.fb.group(
+      {
+        _id: [''],
+        subjectId: ['', Validators.required],
+        systemId: ['', Validators.required],
+        subsystemId: ['', Validators.required],
+        type0: false,
+        type1: false,
+        type2: false,
+        type3: false,
+        type4: false,
+        questionsCount: [
+          '',
+          [
+            Validators.required,
+            this.maxQuestionsValidator,
+            Validators.max(10),
+            Validators.min(1),
+          ],
+        ],
+        mode1: false,
+        mode2: false,
+      },
+      {
+        validators: Validators.compose([
+          this.atLeastOneTypeSelectedValidator(),
+          this.atLeastOneModeSelectedValidator(),
+        ]),
+      }
+    );
     this.getAllSubject();
     this.getAllSystem();
     this.getAllSubsystem();
-    this.qbankForm.setValidators(this.atLeastOneTypeSelectedValidator());
   }
+  toggleChanged(selectedMode: string) {
+    this.examMode = '';
+    if (this.examMode === selectedMode) {
+      this.qbankForm.get('mode1').setValue(false);
+      this.qbankForm.get('mode2').setValue(false);
+      this.examMode = '';
+    }
+    this.examMode = selectedMode;
+  }
+
   maxQuestionsValidator(control: AbstractControl): ValidationErrors | null {
     const maxQuestions = 10;
     if (control.value > maxQuestions) {
@@ -94,13 +122,24 @@ export class BuildTestComponent {
     }
     return null;
   }
+  atLeastOneModeSelectedValidator() {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const mode1 = control.get('mode1').value;
+      const mode2 = control.get('mode2').value;
+      if (!(mode1 || mode2)) {
+        return { atLeastOneMode: true };
+      }
+      return null;
+    };
+  }
   atLeastOneTypeSelectedValidator() {
     return (control: AbstractControl): ValidationErrors | null => {
       const type0 = control.get('type0').value;
       const type1 = control.get('type1').value;
       const type2 = control.get('type2').value;
       const type3 = control.get('type3').value;
-      if (!(type0 || type1 || type2 || type3)) {
+      const type4 = control.get('type4').value;
+      if (!(type0 || type1 || type2 || type3 || type4)) {
         return { atLeastOneType: true };
       }
       return null;
@@ -110,13 +149,12 @@ export class BuildTestComponent {
   generateTest() {
     const temp = this.qbankForm.value;
     const typeArray: number[] = [];
-    const selectedModes = temp.modes;
-    for (let i = 0; i <= 3; i++) {
+    for (let i = 0; i <= 4; i++) {
       if (temp[`type${i}`]) {
         typeArray.push(i);
       }
     }
-    this.qbankObject.mode = selectedModes;
+    this.qbankObject.mode = this.examMode;
     this.qbankObject.questionsCount = parseInt(temp.questionsCount, 10);
     this.qbankObject.subjectId = temp.subjectId;
     this.qbankObject.subsystemId = temp.subsystemId;
@@ -139,14 +177,14 @@ export class BuildTestComponent {
   getAllSystem() {
     this.systemService.getAllSystems().subscribe((doc: any) => {
       this.systemList = doc.result;
-      console.log('system', this.systemList);
+      // console.log('system', this.systemList);
     });
   }
   getAllSubject() {
     this.subjectService.getAllTopicMaster().subscribe(
       (res: any) => {
         this.subjectList = res.result;
-        console.log('subject', this.subjectList);
+        // console.log('subject', this.subjectList);
       },
       (err: any) => {
         console.log(err);
@@ -158,196 +196,11 @@ export class BuildTestComponent {
     this.subSystemService.getAllsubSystems().subscribe(
       (res: any) => {
         this.subsystemList = res.result;
-        console.log('subsystem', this.subsystemList);
+        // console.log('subsystem', this.subsystemList);
       },
       (err: any) => {
         console.log(err);
       }
     );
   }
-
-  // @ViewChild('scrollContainer') scrollContainer: ElementRef;
-  // @ViewChild('scrollExplanationContainer')
-  // scrollExplanationContainer: ElementRef;
-  // @ViewChildren(MatCheckbox) checkboxes: QueryList<MatCheckbox>;
-
-  // selectedSystem: string = 'all';
-  // subSystems: string[] = [];
-  // selectedSubSystem: string = 'all';
-
-  // selectedSubjectControl = new FormControl();
-  // selectedSystemControl = new FormControl();
-  // selectedSubSystemControl = new FormControl();
-  // filteredSubjectss: Subject[];
-  // selectedSubject: any;
-  // systemControl = new FormControl();
-  // filteredSystemList: Observable<any[]>;
-  // this.filteredSystemList = this.systemControl.valueChanges.pipe(
-  //   startWith(''), // Start with an empty string
-  //   map((value) => this._filterUniques(value))
-  // );
-  // this.filteredSubSystemOptions = this.subSystemControl.valueChanges.pipe(
-  //   startWith(''),
-  //   map((value) => this._filterSubSystemOptions(value))
-  // );
-
-  // ngAfterViewInit() {
-  //   const scrollbar = Scrollbar.init(this.scrollContainer.nativeElement, {
-  //     // Smooth Scrollbar options go here
-  //   });
-
-  //   this.subjectService.getAllTopicMaster().subscribe(
-  //     (data: any) => {
-  //       this.subjects = data.result || [];
-  //       this.filteredSubjectss = this.subjects.slice();
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching subjects:', error);
-  //     }
-  //   );
-
-  //   this.systemService.getAllSystems().subscribe(
-  //     (data: any) => {
-  //       this.systems = data.result || [];
-  //       // this.filteredSystems = this.selectedSystemControl.valueChanges.pipe(
-  //       //   startWith(''),
-  //       //   map((value) => this._filterSystems(value))
-  //       // );
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching systems:', error);
-  //     }
-  //   );
-
-  //   this.getAllSubSystems();
-
-  //   // Initialize the filter for subjects and systems
-  //   // this.filteredSubjects = this.selectedSubjectControl.valueChanges.pipe(
-  //   //   startWith(''),
-  //   //   map((value) => this._filterSubjects(value))
-  //   // );
-  // }
-
-  // filterSystems(value: any) {
-  //   this.filteredSystems = this.selectedSystemControl.valueChanges.pipe(
-  //     startWith(''),
-  //     map((value) => this._filterSystems(value))
-  //   );
-  // }
-  // filterValue
-
-  // filterSubjects(event: any) {
-  //   const filterValue = event.target.value.toLowerCase();
-  //   this.filteredSubjectss = this.subjects.filter((subject: any) =>
-  //     subject.subject.toLowerCase().includes()
-  //   );
-  // }
-
-  // displayFn(subject: any): string {
-  //   return subject && subject.subject ? subject.subject : '';
-  // }
-
-  // onSubjectSelected(event: MatOptionSelectionChange) {
-  //   this.selectedSubject = event.source.value;
-  // }
-
-  // onSystemChange(selectedSystem: string) {
-  //   if (selectedSystem === 'all') {
-  //     this.getAllSubSystems();
-  //   } else {
-  //     this.subsystemOptions = this.getSubSystemsBySystem(selectedSystem);
-  //   }
-  //   console.log('Selected System:', selectedSystem);
-  //   console.log('Subsystems:', this.subsystemOptions);
-  // }
-
-  // private _filterUniques(value: string): any[] {
-  //   const filterValue = value.toLowerCase();
-  //   if (this.systemList) {
-  //     return this.systemList.filter((item) =>
-  //       item.system.toLowerCase().includes(filterValue)
-  //     );
-  //   } else {
-  //     return [];
-  //   }
-  // }
-
-  // onOptionSelected(event: any) {
-  //   const selectedValue = event.option.value;
-  //   const matchingItems = this.subsystemList.filter(
-  //     (item) => item.systemId.system === selectedValue
-  //   );
-  //   this.subsystemOptions = matchingItems;
-  // }
-
-  // getSubSystemsBySystem(systemId: string): string[] {
-  //   const matchingSubSystems = this.systems.find(
-  //     (system) => system._id === systemId
-  //   );
-  //   return matchingSubSystems ? matchingSubSystems.subSystemId : [];
-  // }
-
-  // private _filterSubSystemOptions(value: string): any[] {
-  //   const filterValue = value.toLowerCase();
-
-  //   return this.subsystemOptions.filter((system) =>
-  //     system.subSystem.toLowerCase().includes(filterValue)
-  //   );
-  // }
-
-  // onSubSystemSelected(event: any) {
-  //   this.selectedSubSystem = event.option.value;
-  //   this.onSubSystemSelectionChange(this.selectedSubSystem);
-  // }
-
-  // onSubSystemSelectionChange(selectedValue: string) {
-  //   console.log('Selected Subsystem: ', selectedValue);
-  // }
-
-  // getAllSubSystems() {
-  //   this.subSystemService.getAllsubSystems().subscribe(
-  //     (data: any) => {
-  //       this.subsystemList = data.result || [];
-  //       this.subsystemOptions = this.subsystemList;
-  //       console.log('Subsystems fetched successfully:', this.subsystemOptions);
-  //     },
-  //     (err: any) => {
-  //       console.error('Error fetching subsystems:', err);
-  //     }
-  //   );
-  // }
-
-  // selectAll(event: any): void {
-  //   const isChecked = event.checked;
-
-  //   if (isChecked) {
-  //     this.checkboxes.forEach((checkbox) => {
-  //       checkbox.checked = true;
-  //     });
-  //   } else {
-  //     this.checkboxes.forEach((checkbox) => {
-  //       checkbox.checked = false;
-  //     });
-  //   }
-
-  //   this.cdr.detectChanges();
-  // }
-
-  // onSystemSelected(selectedSystem: any) {
-  //   this.onSystemChange(selectedSystem);
-  // }
-
-  // private _filterSubjects(value: string): Subject[] {
-  //   const filterValue = value.toLowerCase();
-  //   return this.subjects.filter((subject) =>
-  //     subject.subject.toLowerCase().includes(filterValue)
-  //   );
-  // }
-
-  // private _filterSystems(value: string): System[] {
-  //   const filterValue = value.toLowerCase();
-  //   return this.systems.filter((system) =>
-  //     system.system.toLowerCase().includes(filterValue)
-  //   );
-  // }
 }
