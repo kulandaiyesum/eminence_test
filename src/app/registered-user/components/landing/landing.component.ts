93beb100e54b56faed84a020dff9724fba9cc1e5
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-11-29 09:15:31 +0530

    get api for landing page

--- src/app/registered-user/components/landing/landing.component.ts
+++ src/app/registered-user/components/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ExamRoomService } from '../../service/exam-room.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,17 +8,35 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {
-  constructor(private route: ActivatedRoute) {}
+  roomCode: string;
+  constructor(
+    private route: ActivatedRoute,
+    private examroomService: ExamRoomService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       const otp = params['otp'];
       const email = params['email'];
       console.log(otp);
+      this.roomCode = otp;
       console.log(email);
-
-
       // Now you can use otp and email in your component
+      this.getLandingPageDetails();
     });
+  }
+
+  getLandingPageDetails() {
+    let data={
+      otp:this.roomCode
+    }
+    this.examroomService.getLandingPage(data).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error('Error fetching random code:', error);
+      }
+    );
   }
 }
