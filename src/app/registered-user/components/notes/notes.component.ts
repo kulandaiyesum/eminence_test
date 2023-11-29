e00505a746d66668abeacdc3f622b15fa3126d7f
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-11-29 17:55:35 +0530

    notepad changes

--- src/app/registered-user/components/notes/notes.component.ts
+++ src/app/registered-user/components/notes/notes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-
+import { ExamDataService } from '../../service/exam-data.service';
 
 @Component({
   selector: 'app-notes',
@@ -8,10 +8,27 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./notes.component.scss'],
 })
 export class NotesComponent {
+  textareaContent: string = '';
+  constructor(
+    public dialogRef: MatDialogRef<NotesComponent>,
+    private examdataService:ExamDataService
+    ) {}
 
-  constructor(public dialogRef: MatDialogRef<NotesComponent>) {}
+    ngOnInit(): void {
+      // Retrieve the text content when the component initializes
+      this.restoreTextContent();
+    }
 
   closeDialog() {
-      this.dialogRef.close();
+    this.dialogRef.close();
+  }
+
+   restoreTextContent(): void {
+    const savedContent = this.examdataService.getTextContent();
+    this.textareaContent = savedContent;
+  }
+
+   saveTextContent(): void {
+    this.examdataService.setTextContent(this.textareaContent);
   }
 }
