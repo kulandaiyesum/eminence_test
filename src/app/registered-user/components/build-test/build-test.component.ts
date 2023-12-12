0daf6c4052d30dfda82718bfb339ebb18fa36846
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-12-12 15:51:33 +0530

    Minor changes

--- src/app/registered-user/components/build-test/build-test.component.ts
+++ src/app/registered-user/components/build-test/build-test.component.ts
@@ -361,6 +361,9 @@ export class BuildTestComponent {
     this.examService.getExamDetailsByStudentId(userid).subscribe(
       (response: any) => {
         console.log(response.result);
+        const filteredTImedMode=response.result.filter((item:any)=>item.mode==='TIMED' && item.percentage < 30);
+        console.log(filteredTImedMode );
+
         const filteredSubjects =response.result
           .filter((item: any) => item.mode === 'TIMED' && item.percentage < 30)
           .map((item: any) => item?.subjectId?.subject);
