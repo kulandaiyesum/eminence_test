80baf452a92830de3bfd205d9fc30340ca42a67a
shekshowkathm <shekshowkath2001ms@gmail.com>
2023-12-12 17:17:54 +0530

    Select options changed

--- src/app/registered-user/components/build-test/build-test.component.ts
+++ src/app/registered-user/components/build-test/build-test.component.ts
@@ -372,10 +372,12 @@ export class BuildTestComponent {
     this.examService.getExamDetailsByStudentId(userid).subscribe(
       (response: any) => {
         console.log(response.result);
-        const filteredTImedMode=response.result.filter((item:any)=>item.mode==='TIMED' && item.percentage < 30);
-        console.log(filteredTImedMode );
+        const filteredTImedMode = response.result.filter(
+          (item: any) => item.mode === 'TIMED' && item.percentage < 30
+        );
+        console.log(filteredTImedMode);
 
-        const filteredSubjects =response.result
+        const filteredSubjects = response.result
           .filter((item: any) => item.mode === 'TIMED' && item.percentage < 30)
           .map((item: any) => item?.subjectId?.subject);
 
