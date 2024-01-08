da3eae148305422a1ef6ffd0b347014b69212514
kulandaiyesum <kulandaiyesu333@gmail.com>
2024-01-08 11:59:22 +0530

    style: header responsive minor bug fix

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from 'src/app/login/components/login/login.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private breakpointObserverSubscription: Subscription;
   isMenuDisply!: boolean;
   currentRoute: string = '';
-  constructor(private router: Router, public dialog: MatDialog) {
+  isScreenMath: boolean = false;
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    private breakpointObserver: BreakpointObserver
+  ) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         if (
@@ -27,7 +35,22 @@ export class HeaderComponent implements OnInit {
       }
     });
   }
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    if (this.breakpointObserverSubscription) {
+      this.breakpointObserverSubscription.unsubscribe();
+    }
+  }
+  ngOnInit(): void {
+    this.breakpointObserverSubscription = this.breakpointObserver
+      .observe('(max-width: 820px)')
+      .subscribe((result) => {
+        if (result.matches) {
+          this.isScreenMath = true;
+        } else {
+          this.isScreenMath = false;
+        }
+      });
+  }
 
   openLoginPopUp() {
     const dialogRef = this.dialog.open(LoginComponent, {
